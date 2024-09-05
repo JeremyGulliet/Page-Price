984680f5839679e1c1ae5f905e94477719bd0e6d
Jeremy GULLIET <jrmy.gulliet@gmail.com>
2024-09-05 15:15:46 +0200

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
+/* eslint-disable react/no-unescaped-entities */
+"use client"
+
+import CardPricesMonth from "@/components/cardPrice/CardPricesMonth";
+import CardPricesYear from "@/components/cardPrice/CardPricesYear";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [activeButton, setActiveButton] = useState("monthly");
+
+  return (
+    <main className="flex justify-center w-full h-full  bg-gradient-to-tr from-blue-900 to-blue-950 text-white">
+      <div className="flex flex-col justify-start items-center">
+        <h6 className="flex items center justify-center bg-gradient-to-r from-[#0738B966] to-[#03195366] w-[150px] mt-[34px] p-1 rounded-xl border border-white/50">
+          Nos offres
+        </h6>
+        <h1 className="flex flex-col justify-center items-center text-5xl font-semibold mt-7">
+          Des prix en accord avec <br />
+          <span className="text-transparent bg-clip-text bg-[radial-gradient(44.52%_83.97%_at_55.31%_103.97%,rgba(4,59,203,1)_28%,rgba(136,179,253,1)_100%)]  w-max">
+            notre vision
+          </span>
+        </h1>
+        <div className="flex justify-center items-center gap-4 mt-6">
+          <Button
+            onClick={() => setActiveButton("monthly")}
+            className={`w-[275px] h-[45px] rounded-xl transition ease-in-out duration-150 hover:scale-105 ${
+              activeButton === "monthly"
+                ? "bg-[radial-gradient(51.41%_105.14%_at_50.04%_105.14%,rgba(10,48,139,1)_0%,rgba(30,110,251,1)_100%)]"
+                : "bg-transparent border border-blue-500"
+            }`}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Paiement Mensuel
+          </Button>
+
+          <Button
+            onClick={() => setActiveButton("annual")}
+            className={`w-[275px] h-[45px] rounded-xl transition ease-in-out duration-150 hover:scale-105 ${
+              activeButton === "annual"
+                ? "bg-[radial-gradient(51.41%_105.14%_at_50.04%_105.14%,rgba(10,48,139,1)_0%,rgba(30,110,251,1)_100%)]"
+                : "bg-transparent border border-blue-500"
+            }`}
           >
-            Read our docs
-          </a>
+            Paiement Annuel (-20%)
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {activeButton === "monthly" ? <CardPricesMonth /> : <CardPricesYear />}
+      </div>
+    </main>
   );
 }
---
+++ src/components/cardPrice/CardPricesMonth.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import data from "../../data.json";
+import { Button } from "../ui/button";
+import { IoCheckmark } from "react-icons/io5";
+import { IoDiamondOutline } from "react-icons/io5";
+import { TbTargetArrow } from "react-icons/tb";
+
+export default function CardPricesMonth() {
+  const [currency, setCurrency] = useState("EUR");
+
+  // Fonction pour convertir le prix
+  const convertPrice = (priceInEuros: number) => {
+    switch (currency) {
+      case "USD":
+        return (priceInEuros * 1.1).toFixed(2); // Exemple de taux de conversion
+      case "GBP":
+        return (priceInEuros * 0.85).toFixed(2);
+      default:
+        return priceInEuros;
+    }
+  };
+
+  // Fonction pour obtenir le symbole de la devise
+  const getCurrencySymbol = () => {
+    switch (currency) {
+      case "USD":
+        return "$";
+      case "GBP":
+        return "£";
+      default:
+        return "€";
+    }
+  };
+
+  return (
+    <section className="flex flex-col justify-center items-center w-screen my-10">
+      <div className="flex gap-4 mb-6">
+        <Button
+          className=" w-24 bg-[radial-gradient(51.41%_105.14%_at_50.04%_105.14%,rgba(10,48,139,1)_0%,rgba(30,110,251,1)_100%)] rounded-xl transition ease-in-out duration-150 hover:scale-105 text-base"
+          onClick={() => setCurrency("EUR")}
+        >
+          EUR (€)
+        </Button>
+        <Button
+          className=" w-24 bg-[radial-gradient(51.41%_105.14%_at_50.04%_105.14%,rgba(10,48,139,1)_0%,rgba(30,110,251,1)_100%)] rounded-xl transition ease-in-out duration-150 hover:scale-105 text-base"
+          onClick={() => setCurrency("USD")}
+        >
+          USD ($)
+        </Button>
+        <Button
+          className=" w-24 bg-[radial-gradient(51.41%_105.14%_at_50.04%_105.14%,rgba(10,48,139,1)_0%,rgba(30,110,251,1)_100%)] rounded-xl transition ease-in-out duration-150 hover:scale-105 text-base"
+          onClick={() => setCurrency("GBP")}
+        >
+          GBP (£)
+        </Button>
+      </div>
+
+      <div className="flex justify-center gap-6">
+        {data &&
+          data.map((e) => (
+            <Card
+              key={e.id}
+              className={
+                e.title !== "Ultime"
+                  ? "flex flex-col justify-between w-[375px] h-[650px]  bg-[radial-gradient(103.02%_79.39%_at_50.04%_100%,rgba(3,25,83,1)_0%,rgba(23,112,247,1)_120%)]"
+                  : "flex flex-col justify-between w-[403px] h-[750px]  bg-gradient-to-l from-blue-400 to-blue-700"
+              }
+            >
+              <CardHeader>
+                {e.title === "Ultime" ? (
+                  <CardTitle className="flex flex-col gap-4 text-3xl">
+                    <IoDiamondOutline />
+                    <div className="flex items-center gap-4">
+                      {e.title}
+                      <p className="text-sm bg-blue-300/50 rounded-xl py-1 px-2">
+                        Best Offer
+                      </p>
+                    </div>
+                  </CardTitle>
+                ) : (
+                  <CardTitle className="flex flex-col gap-4 text-3xl">
+                    <TbTargetArrow />
+                    {e.title}
+                  </CardTitle>
+                )}
+                <CardDescription className="border-b-2 pt-4 pb-8 text-xl">
+                  <span className="text-5xl">
+                    {getCurrencySymbol()}
+                    {convertPrice(e.priceMonth)}
+                  </span>{" "}
+                  /mois
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                {e.included.map((item) => (
+                  <ul key={item}>
+                    <li className="flex items-center text-lg gap-[5px] leading-[45px]">
+                      <IoCheckmark />
+                      {item}
+                    </li>
+                  </ul>
+                ))}
+              </CardContent>
+              <CardFooter className="flex justify-center items-center">
+                <Button className=" w-80 bg-[radial-gradient(51.41%_105.14%_at_50.04%_105.14%,rgba(10,48,139,1)_0%,rgba(30,110,251,1)_100%)] rounded-xl transition ease-in-out duration-150 hover:scale-105 text-lg">
+                  Je m'abonne
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
+    </section>
+  );
+}
